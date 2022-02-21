e8cc0c742341fba1cbff83ad4c640a9404612833
xatier <xatierlike@gmail.com>
2022-02-21 17:03:07 -0600

    Support mobile.twitter.com domain

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ chrome.tabs.query({ currentWindow: true, highlighted: true }, async (tab) => {
     const site = 'https://pawoo.net'
     const nl = '\n\n'
 
-    const TWITTER_STATUS_REGEX = /^https:\/\/twitter.com\/(.+?)\/status\/(\d+)/
+    const TWITTER_STATUS_REGEX =
+        /^https:\/\/(mobile\.)?twitter.com\/(.+?)\/status\/(\d+)/
 
     let intent = `${site}/share?text=${encodeURIComponent(
         nl + title + nl + url,
